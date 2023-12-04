44d15aede00b98eb37c98424f04f2ced827277fe
StikJoy <arslanlatypov1@mail.ru>
2023-12-04 16:32:29 +0300

    Add horizontally banner, ty page and ty style

---
+++ my-app/src/components/HorizontallyBanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../styles/containerStyle.css";
+import "../styles/carouselStyle.css";
+
+const HorizontallyBanner = function () {
+    return (
+        <div>
+            <div className="container">
+                <div className="banner">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HorizontallyBanner;
---
+++ my-app/src/components/Ty.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "../styles/tyStyle.css"
+
+const Ty = function () {
+    return (
+        <div>
+            <div className="container">
+                <div className="tyElements">
+                    <img src="#" alt="#"/>
+                    <h1>Спасибо за заказ</h1>
+                    <p>Наш менеджер скоро свяжется с вами!</p>
+                    <button>Вернуться в магазин</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Ty;
